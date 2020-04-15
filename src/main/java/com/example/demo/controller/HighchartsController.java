f0579554e9f5aba4bdb6511b76247e7a41884202
zhangzhognhui <1352216469@qq.com>
2020-04-15 18:25:12 +0800

    修复 highcharts-export-server 导出 乱码问题

--- src/main/java/com/example/demo/controller/HighchartsController.java
+++ src/main/java/com/example/demo/controller/HighchartsController.java
@@ -1,36 +1,20 @@
 package com.example.demo.controller;
 
 import com.alibaba.fastjson.JSONObject;
-import org.apache.http.HttpEntity;
-import org.apache.http.ParseException;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.util.EntityUtils;
-import org.apache.logging.log4j.util.Strings;
-import org.omg.CORBA.Environment;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sun.misc.BASE64Decoder;
 
 import java.io.*;
-import java.net.URLEncoder;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-import java.util.ArrayList;
 import java.util.Base64;
-import java.util.List;
 import java.util.UUID;
 
 /**
@@ -81,11 +65,19 @@ public class HighchartsController {
         //生成为id
         String uuid=UUID.randomUUID().toString().replaceAll("-","");
         String inFileName="./"+uuid+".json";
-        writeFileState(inFileName,json);
+        try {
+            writeFileState(inFileName,json);
+        } catch (IOException e) {
+            logger.error("IO关闭异常",e);
+            result.put("code",500);
+            result.put("msg","文件读取失败");
+            return result;
+        }
 
         synchronized (this){
             String outfileName=outFilePath+uuid+suffix;
-            String cmd = "highcharts-export-server --infile "+inFileName+" --outfile "+outfileName;
+            String cmd = "highcharts-export-server --type png --infile "+inFileName+" --outfile "+outfileName;
+            ByteArrayOutputStream byteOutStream=null;
             try {
                 Runtime run = Runtime.getRuntime();
                 Process p = run.exec(cmd);
@@ -100,9 +92,10 @@ public class HighchartsController {
                 inBr.close(); in .close();
 
                 //将文件转换 base64 字符串
-                byte[] b = Files.readAllBytes(Paths.get(outfileName));
+                byteOutStream = readFileState(outfileName);
+                String base64=Base64.getEncoder().encodeToString(byteOutStream.toByteArray());
 
-                result.put("data",Base64.getEncoder().encodeToString(b));
+                result.put("data",base64);
                 result.put("code",200);
                 result.put("msg","success");
                 return result;
@@ -116,6 +109,9 @@ public class HighchartsController {
                 try {
                     Files.delete(Paths.get(outfileName));
                     Files.delete(Paths.get(inFileName));
+                    if (byteOutStream != null) {
+                        byteOutStream.close();
+                    }
                 } catch (IOException e) {
                     logger.error("文件删除失败",e);
                     result.put("code",500);
@@ -127,24 +123,60 @@ public class HighchartsController {
 
     }
 
-    public static void writeFileState(String fileName, String text) {
+    /**
+     * 将文件转换成 ByteArrayOutputStream
+     * @param path
+     * @return
+     * @throws IOException
+     */
+    public static ByteArrayOutputStream readFileState(String path) throws IOException {
+        String str = "";
+        BufferedInputStream buff;
+        ByteArrayOutputStream output = null;
+        try {
+            buff = new BufferedInputStream(new FileInputStream(path));
+            byte[] bytes=new byte[1024];
+            int line;
+            output = new ByteArrayOutputStream();
+            while( (line = buff.read(bytes)) != -1) {
+                output.write(bytes,0,line);
+            }
+            buff.close();
+        }catch(IOException e) {
+            e.printStackTrace();
+        }
+        return output;
+    }
+
+    /**
+     * niod的方式写入文件
+     * @param fileName
+     * @param text
+     * @throws IOException
+     */
+    public static void writeFileState(String fileName, String text) throws IOException {
         FileOutputStream fileOutputStream = null;
         try {
             fileOutputStream = new FileOutputStream(new File(fileName));
             FileChannel fileChannel = fileOutputStream.getChannel();
-            ByteBuffer byteBuffer = Charset.forName("GBK").encode(text);
+            ByteBuffer byteBuffer = Charset.forName("UTF-8").encode(text);
             int length = 0;
             try {
                 while ((length = fileChannel.write(byteBuffer)) != 0) {
-                    System.out.println("file length have written: " + length);
+                    logger.info("file length have written: " + length);
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             }
         } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            logger.error("写入文件异常： ",e);
+        }finally {
+            if (fileOutputStream != null) {
+                fileOutputStream.close();
+            }
         }
 
     }
+
 
 }
